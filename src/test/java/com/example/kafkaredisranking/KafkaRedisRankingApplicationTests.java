84700bb3d4ae38d85a77f314f0f69c285fa54fb0
gominnam <kosok03@naver.com>
2024-03-25 03:58:05 +0900

    init kafka-redis-ranking

---
+++ src/test/java/com/example/kafkaredisranking/KafkaRedisRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaredisranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRedisRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
